4aee9a6acf442669e7fe51076524fb6f582557b0
Candykiller16 <Candykiller16@mail.ru>
2022-03-08 13:38:50 +0300

    first commit

---
+++ game_stop.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+with open('test.txt', 'w') as test:
+    page = 1
+
+    while True:
+        r = requests.get("https://stopgame.ru/review/new/izumitelno/p" + str(page))
+        html = BS(r.content, 'html.parser')
+        items = html.select(".items > .article-summary")
+
+        if len(items):
+            for el in items:
+                title = el.select('.caption > a')
+                test.write(title[0].text + '\n')
+            page += 1
+        else:
+          break
---
+++ stop_game_parse.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import json
+
+HOST = 'https://stopgame.ru'
+URL = 'https://stopgame.ru/review/new/izumitelno/p'
+CSV = 'cards.csv'
+
+def get_html(url, params=''):
+    r = requests.get(url, params=params)
+    return r
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', attrs={'class': 'item article-summary'})
+    games = []
+    # print(items)
+    for item in items:
+        games.append(
+            {
+                'title': item.find('div', class_='article-description').find('div', class_='caption caption-bold').get_text(strip=True),
+                'link_img': item.find('a', class_='article-image image').find('img').get('src'),
+                'date': item.find('div', class_='article-description').find('div', class_='info').find('span', class_='info-item timestamp').get_text().replace(u'\xa0', u' '),
+                'link_article': HOST + item.find('div', class_='article-description').find('div', class_='caption caption-bold').find('a').get('href')
+            }
+        )
+    return games
+
+def save_doc(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Название статьи', 'Ссылка на картинку', 'Дата публикации', 'Ссылка на статью'])
+        for item in items:
+            writer.writerow( [item['title'], item['link_img'], item['date'], item['link_article']])
+
+
+def parser():
+    PAGENATION = input('Укажите количество страниц для парсинга: ')
+    PAGENATION = int(PAGENATION.strip())
+    html = get_html(URL)
+    if html.status_code == 200:
+        cards = []
+        for page in range(1, PAGENATION+1):
+            print(f'Парсим страницу - {page}')
+            html = get_html(URL + str(page))
+            cards.extend(get_content(html.text))
+            # save_doc(cards, CSV)
+        print('Парсинг закончился')
+        return cards
+    else:
+        print('Error')
+
+
+def into_csv():
+    data = parser()
+    save_doc(data, CSV)
+
+
+
+def into_json():
+    value = parser()
+    data = json.dumps(value,sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
+    with open('file.json', 'w', encoding='utf-8') as file:
+        file.write(data)
+
